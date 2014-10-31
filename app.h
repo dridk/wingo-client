556e773b96050bf87f56142a58c89173fd9cdfc1
Sacha schutz <sacha@labsquare.org>
2014-10-31 22:31:39 +0100

    first commit hackathon !!

---
+++ app.h
@@ -0,0 +1,18 @@
+#ifndef APP_H
+#define APP_H
+
+#include <QObject>
+
+class App : public QObject
+{
+    Q_OBJECT
+public:
+    explicit App(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // APP_H
