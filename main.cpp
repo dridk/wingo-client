e2ab48714c4d7cc6a75665b433d8bd935c5666ab
Sacha schutz <sacha@labsquare.org>
2014-10-31 22:36:59 +0100

    add version name

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setApplicationName("Wingo - Xu Fu");
+    app.setApplicationVersion("0.1.0-alpha");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
